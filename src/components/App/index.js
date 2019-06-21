53f5c1bd7e37d9d91d5a4ef9659b044d6d794220
vanita5 <mail@vanit.as>
2019-06-21 03:12:05 +0200

    refactor(src): refactor src files, introduce components dir

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import './App.scss'
-
 class App extends React.Component {
     render() {
         return (
             <div>
-              <h1>liv5</h1>
+                <h1>liv5</h1>
             </div>
         )
     }
