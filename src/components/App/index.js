1ebe2ac735e5dfb4635dc96be26bbcb2da5132d9
vanita5 <mail@vanit.as>
2019-06-21 13:23:01 +0200

    chore(router): add router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>liv5</h1>
             </div>
+            /*<Router>
+                <Layout>
+                    <Route ... />
+                </Layout>
+            </Router>
+            */
         )
     }
 }
