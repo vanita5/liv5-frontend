a35b33013f245ee7d4119f5274cfdd218199ee79
vanita5 <mail@vanit.as>
2019-06-26 21:26:55 +0200

    feat(todos): implement todo click

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import moment from 'moment'
 import { connect } from 'react-redux'
 import { List, Checkbox } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 import { getTodosAsync } from '../../action/liv5'
+
+import * as ROUTES from '../../constants/routes'
 
 class Todos extends React.Component {
     componentDidMount() {
@@ -16,7 +19,7 @@ class Todos extends React.Component {
                 <h1>Todos</h1>
                 <List divided selection relaxed>
                     {this.props.todos.map(todo => (
-                        <List.Item key={todo.todo_id}>
+                        <List.Item key={todo.todo_id} as={Link} to={ROUTES.TODO(todo.todo_id)}>
                             <List.Icon name="check circle outline" size="large" verticalAlign="middle" />
                             <List.Content>
                                 <List.Header as="a">{todo.title}</List.Header>
