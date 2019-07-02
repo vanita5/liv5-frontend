81d0444b28ac744e3e65a28fc9e9b12cdad70328
vanita5 <mail@vanit.as>
2019-07-02 14:44:23 +0200

    fix(todo): type errors

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -28,10 +28,10 @@ class Todos extends React.Component {
                             <List.Icon name="check circle outline" size="large" verticalAlign="middle" />
                             <List.Content>
                                 <List.Header as="p" className="todo-header">{todo.title}</List.Header>
-                                <List.Description as="p">
+                                <List.Description>
                                     Updated {moment(todo.updated_at).fromNow()}
                                     {todo.labels.map(label => (
-                                        <Label color={label.color} horizontal>
+                                        <Label color={label.color} key={label.label_id} horizontal>
                                             <Icon name={label.icon} />
                                             {label.name}
                                         </Label>
